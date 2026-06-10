a9762e0dcd349b954eaa0ef3a95dd4685c81d11a
jjkao <jjkao@usc.edu>
2026-06-10 15:11:28 -0700

    fixed bound check

--- randomtest_dense_skew.cpp
+++ randomtest_dense_skew.cpp
@@ -31,17 +31,17 @@ struct TrialResult {
   long time_slow;
 };
 
-constexpr static auto policy_naive = [](auto &bpool, const auto &,
-                                        auto &kpool) -> SkewOperator {
-  if (!bpool.empty()) {
-    auto ret = bpool.back();
-    bpool.pop_back();
-    return ret;
-  }
-  auto ret = kpool.back();
-  kpool.pop_back();
-  return ret;
-};
+// constexpr static auto policy_naive = [](auto &bpool, const auto &,
+//                                         auto &kpool) -> SkewOperator {
+//   if (!bpool.empty()) {
+//     auto ret = bpool.back();
+//     bpool.pop_back();
+//     return ret;
+//   }
+//   auto ret = kpool.back();
+//   kpool.pop_back();
+//   return ret;
+// };
 
 constexpr static auto policy_random = [](auto &bpool, const auto &,
                                          auto &kpool) -> SkewOperator {
@@ -70,37 +70,37 @@ constexpr static auto policy_random = [](auto &bpool, const auto &,
   return ret;
 };
 
-// constexpr static auto policy = [](auto &bpool, const auto &ret,
-//                                   auto &kpool) -> SkewOperator {
-//   QSpace bspace = ret.GetBraSpace();
-//   QSpace kspace = ret.GetKetSpace();
-//   size_t b_max_overlap{0uz};
-//   size_t b_curr_best{0uz};
-//   for (auto i{0uz}; i < bpool.size(); ++i) {
-//     size_t overlap = (bpool[i].GetBraSpace() & bspace).count();
-//     if (overlap > b_max_overlap) {
-//       b_max_overlap = overlap;
-//       b_curr_best = i;
-//     }
-//   }
-//   size_t k_max_overlap{0uz};
-//   size_t k_curr_best{0uz};
-//   for (auto i{0uz}; i < kpool.size(); ++i) {
-//     size_t overlap = (kpool[i].GetKetSpace() & kspace).count();
-//     if (overlap > k_max_overlap) {
-//       k_max_overlap = overlap;
-//       k_curr_best = i;
-//     }
-//   }
-//   if (b_curr_best > k_curr_best) {
-//     SkewOperator ret1 = bpool[b_curr_best];
-//     bpool.erase(bpool.begin() + static_cast<std::ptrdiff_t>(b_curr_best));
-//     return ret1;
-//   }
-//   SkewOperator ret1 = kpool[k_curr_best];
-//   kpool.erase(kpool.begin() + static_cast<std::ptrdiff_t>(k_curr_best));
-//   return ret1;
-// };
+constexpr static auto policy_overlap = [](auto &bpool, const auto &ret,
+                                          auto &kpool) -> SkewOperator {
+  QSpace bspace = ret.GetBraSpace();
+  QSpace kspace = ret.GetKetSpace();
+  size_t b_max_overlap{0uz};
+  size_t b_curr_best{0uz};
+  for (auto i{0uz}; i < bpool.size(); ++i) {
+    size_t overlap = (bpool[i].GetBraSpace() & bspace).count();
+    if (overlap > b_max_overlap) {
+      b_max_overlap = overlap;
+      b_curr_best = i;
+    }
+  }
+  size_t k_max_overlap{0uz};
+  size_t k_curr_best{0uz};
+  for (auto i{0uz}; i < kpool.size(); ++i) {
+    size_t overlap = (kpool[i].GetKetSpace() & kspace).count();
+    if (overlap > k_max_overlap) {
+      k_max_overlap = overlap;
+      k_curr_best = i;
+    }
+  }
+  if (b_curr_best > k_curr_best || kpool.empty()) {
+    SkewOperator ret1 = bpool[b_curr_best];
+    bpool.erase(bpool.begin() + static_cast<std::ptrdiff_t>(b_curr_best));
+    return ret1;
+  }
+  SkewOperator ret1 = kpool[k_curr_best];
+  kpool.erase(kpool.begin() + static_cast<std::ptrdiff_t>(k_curr_best));
+  return ret1;
+};
 
 template <typename ResultType> struct TimedResult {
   long time;
@@ -166,10 +166,10 @@ auto do_test(const TrialArgs &args) -> TrialResult {
   auto conj_clone = Clone(conj);
 
   auto [time_fast_1, inner_fast_op_1] =
-      time_in_ms([&]() { return Inner(random_bra, conj, policy_naive); });
+      time_in_ms([&]() { return Inner(random_bra, conj, policy_random); });
 
   auto [time_fast_2, inner_fast_op_2] =
-      time_in_ms([&]() { return Inner(random_bra, conj, policy_random); });
+      time_in_ms([&]() { return Inner(random_bra, conj, policy_overlap); });
 
   complex inner_slow;
   auto [time_slow] =
